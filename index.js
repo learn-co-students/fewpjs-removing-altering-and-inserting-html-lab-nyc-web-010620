481d133e04f57788e3e4747938860aa452f5ba77
Kate Raskauskas <katherine.raskauskas@gmail.com>
2020-02-18 20:38:22 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+
+
+
+let main = document.getElementById('main')
+main.remove()
+
+let newHeader = document.createElement('h1')
+newHeader.id = "victory"
+document.body.appendChild(newHeader);
+newHeader.innerHTML = '<h1 id="victory">KATE is the champion</h1>'
